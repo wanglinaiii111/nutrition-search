f85a8cbdc57594cd5c6567ba5ac1f3f3c349eeb0
wdpl <811197627@qq.com>
2020-11-16 10:42:42 +0800

    test

--- src/pages/food/listItem/listItem.jsx
+++ src/pages/food/listItem/listItem.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from '@tarojs/components'
 import styles from './listItem.module.scss'
 import { useSelector } from 'react-redux';
 
 const ListItem = React.memo((props) => {
   const { data, clickToDetail } = props;
+  const [dataMap,set_dataMap] = useState([])
   const elementMap = useSelector(state => state.food.elementMap)
   const click = () => {
     clickToDetail()
   }
+
+  useEffect(()=>{
+    set_dataMap(Object.keys(data.eles))
+  },[])
 
   return <View className={styles.container} onClick={click}>
     <View className={styles.content}>
@@ -17,19 +22,22 @@ const ListItem = React.memo((props) => {
     </View>
     <View className={styles.list}>
       {
-        Object.keys(data.eles)
+        dataMap
           .map(item => {
             return <>
               <View
                 className={styles.listItem}
               >
-                <View className={styles.elementName}>{
+                {
+                  item
+                }
+                {/* <View className={styles.elementName}>{
                   elementMap.map(e => {
                     if (e.code === item) {
                       return e.name
                     }
                   })
-                }</View>
+                }</View> */}
                 <View className={styles.zhi}>
                   {data.eles[item]}
                 </View>
